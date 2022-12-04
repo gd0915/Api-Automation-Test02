a5f430045f80abfc5b2eee6d02a4d423fc8be440
Hatice Erzincan Sozeri <haticeesozeri@gmail.com>
2022-12-04 18:45:07 -0500

    New Test Files Added

---
+++ src/test/java/base_urls/DummyRestApiBaseUrl.java
@@ -0,0 +1,17 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DummyRestApiBaseUrl {
+
+    protected RequestSpecification spec;
+
+    @BeforeEach//JunitJupiter annotation/JunitJupiter dependency new feature
+    public void setUp(){
+
+        spec= new RequestSpecBuilder().setBaseUri("https://dummy.restapiexample.com/api/v1").build();
+    }
+
+}
---
+++ src/test/java/pojos/PetStorePetPojo.java
@@ -0,0 +1,90 @@
+package pojos;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PetStorePetPojo {
+
+    private int id;
+    private Category category;
+    private String name;
+    private ArrayList<String> photoUrls;
+    private ArrayList<Tags> tags;
+    private String status;
+
+
+
+    public PetStorePetPojo(int id, Category category, String name, ArrayList<String> photoUrls, ArrayList<Tags> tags, String status) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+        this.photoUrls = photoUrls;
+        this.tags = tags;
+        this.status = status;
+    }
+
+    public PetStorePetPojo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(ArrayList<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public ArrayList<Tags> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<Tags> tags) {
+        this.tags = tags;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "PetStorePetPojo{" +
+                "id=" + id +
+                ", category=" + category +
+                ", name='" + name + '\'' +
+                ", photoUrls=" + photoUrls +
+                ", tags=" + tags +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
